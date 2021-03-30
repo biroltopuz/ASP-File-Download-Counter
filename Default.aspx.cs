42c37f512211ccbbb260c54da00a0b6603bba8d0
Birol Topuz <birolnet@gmail.com>
2021-03-30 22:02:42 +0300

    First commit

---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_FileDownloadCounter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // string fileName = "csharp.png"; // test için
+            string fileName = Context.Request["file"];
+
+            if (fileName.ToString() != "")
+            {
+                WriteToLog(fileName);
+                Download(fileName);
+            }
+        }
+
+        void Download(string fileName)
+        {
+            FileInfo fi;
+            long n;
+            fi = new FileInfo(MapPath("File/" + fileName));
+            n = fi.Length;
+            Response.Buffer = false;
+            Response.Clear();
+            Response.ClearContent();
+            Response.ClearHeaders();
+            //Response.ContentType = "application/x-zip-compressed"; // zip için
+            Response.ContentType = "image/png"; // image için
+            Response.AppendHeader("content-disposition", "attachment;filename=" + fileName);
+            Response.AppendHeader("Content-Length", Convert.ToString(n));
+            Response.TransmitFile("File/" + fileName);
+        }
+
+        private void WriteToLog(string Message)
+        {
+            string logFileName = AppDomain.CurrentDomain.BaseDirectory + "Log\\Log_" + DateTime.Now.Date.ToShortDateString().Replace("/", "_") + ".txt";
+            using (StreamWriter sw = File.AppendText(logFileName))
+            {
+                sw.WriteLine(DateTime.Now + " => " + Message);
+            }
+        }
+    }
+}
